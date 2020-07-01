85f4749019f7613b6934cf72d1230ed6d0b30e78
Grigorii Raikov <griray090494@gmail.com>
2020-07-01 13:47:15 +0300

    Initial commit

---
+++ json_format.py
@@ -0,0 +1,34 @@
+import json
+from pprint import pprint
+from collections import Counter
+
+
+with open("newsafr.json", encoding="utf-8") as f:
+
+    all_words = []
+    pop_words = []
+
+    json_data = json.load(f)
+    for item in json_data['rss']['channel']['items']:
+        news = item['description'].split()
+        all_words.extend(news)
+        for word in all_words:
+            if len(word) > 6:
+                pop_words.append(word)
+
+    stat = Counter(pop_words)
+    sort_stat = sorted(((say, number) for say, number in stat.items()), key=lambda pair: pair[1], reverse=True)
+    print("Топ 10 слов в новостной ленте: ")
+    for i in range (10):
+        print(sort_stat[i])
+
+
+
+
+
+
+
+
+
+
+
