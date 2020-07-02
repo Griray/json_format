4aa8bfc760bd1afbe0ae292902d20122406b845a
Grigorii Raikov <griray090494@gmail.com>
2020-07-02 19:57:50 +0300

    Correction of algoritm

--- json_format.py
+++ json_format.py
@@ -10,25 +10,15 @@ with open("newsafr.json", encoding="utf-8") as f:
 
     json_data = json.load(f)
     for item in json_data['rss']['channel']['items']:
+        news = item['description'].lower()
         news = item['description'].split()
         all_words.extend(news)
-        for word in all_words:
-            if len(word) > 6:
-                pop_words.append(word)
+    for word in all_words:
+        if len(word) > 6:
+            pop_words.append(word)
 
     stat = Counter(pop_words)
     sort_stat = sorted(((say, number) for say, number in stat.items()), key=lambda pair: pair[1], reverse=True)
     print("Топ 10 слов в новостной ленте: ")
     for i in range (10):
         print(sort_stat[i])
-
-
-
-
-
-
-
-
-
-
-
