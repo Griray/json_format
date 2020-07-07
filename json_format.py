5ed0182178e8757491c477903bcfb53b4ef9fc18
Grigorii Raikov <griray090494@gmail.com>
2020-07-07 14:31:45 +0300

    correction of lower

--- json_format.py
+++ json_format.py
@@ -10,15 +10,17 @@ with open("newsafr.json", encoding="utf-8") as f:
 
     json_data = json.load(f)
     for item in json_data['rss']['channel']['items']:
-        news = item['description'].lower()
-        news = item['description'].split()
+        news = item['description']
+        news = news.lower()
+        news = news.split()
+
+
         all_words.extend(news)
+
     for word in all_words:
         if len(word) > 6:
             pop_words.append(word)
 
     stat = Counter(pop_words)
-    sort_stat = sorted(((say, number) for say, number in stat.items()), key=lambda pair: pair[1], reverse=True)
-    print("Топ 10 слов в новостной ленте: ")
-    for i in range (10):
-        print(sort_stat[i])
+    print("ТОП 10 слов", stat.most_common(10))
+
